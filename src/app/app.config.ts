ce5a51e95e322fd595ef69ca678df62ddf773184
Shannon <37170995+shanny210@users.noreply.github.com>
2024-08-16 19:13:07 +0200

    Auto-generated commit

--- src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import {ApplicationConfig, importProvidersFrom} from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MessageService} from "primeng/api";
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), [importProvidersFrom(BrowserAnimationsModule)], MessageService]
-};
